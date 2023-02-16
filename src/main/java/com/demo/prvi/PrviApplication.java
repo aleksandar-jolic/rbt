c555ef19cc65dbec63a9e9e68ea3bb0c4a1bafb8
Aleksandar Jolic <coajolic@gmail.com>
2023-02-16 11:25:49 +0100

    Dodao sam promenu za main klasu

--- src/main/java/com/demo/prvi/PrviApplication.java
+++ src/main/java/com/demo/prvi/PrviApplication.java
@@ -8,6 +8,8 @@ public class PrviApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrviApplication.class, args);
+                
+                System.out.println("Promena");
 	}
 
 }
