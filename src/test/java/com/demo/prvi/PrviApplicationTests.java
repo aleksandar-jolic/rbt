108dc6a50156a84196f4cd907d5a2681c7410e7c
Aleksandar Jolic <coajolic@gmail.com>
2023-02-16 11:15:44 +0100

    Git hub demo project

---
+++ src/test/java/com/demo/prvi/PrviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.prvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
